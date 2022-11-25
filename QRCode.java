029e9960c5340b5d308c86dc22a25add4da9378a
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2022-11-25 11:33:37 +0200

    Add files via upload

---
+++ QRCode.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class QRCode {
+    public static void QRCodeGeneration(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix matrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(matrix, "JPG", path);
+    }
+}
